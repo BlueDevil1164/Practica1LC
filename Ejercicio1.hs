bd85cacc9c8248996553a6bd41011c8da6dc92e1
BlueDevil1164 <73565735+BlueDevil1164@users.noreply.github.com>
2020-11-20 19:26:13 -0600

    Delete Ejercicio1.hs

--- Ejercicio1.hs
+++
@@ -1,7 +0,0 @@
---Variables ---------------------------------------------------------
---Un tipo de datos para variables:
-type Variable= String
---OTRAS opciones para definir Variable:
-type Variable2= Int
-data Variable3= X | V Variable3 deriving (Eq,Show)
-data Variable4= X0 | X1 | X2 | X3 | X4 | X5 | X6 | X7 | X8 | X9 deriving (Eq,Show) --Diez variables
